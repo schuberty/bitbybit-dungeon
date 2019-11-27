76f1fd6d73c828e171b00e52215fa4eef5604e4b
schubert <schubertyi@gmail.com>
2019-11-27 01:05:02 -0300

    Clear all registers

--- src/bitbybit-dungeon.asm
+++ src/bitbybit-dungeon.asm
@@ -1,17 +1,49 @@
-.globl main,stop
-.globl playerName,playerColor
+.globl main,stop,zeroAll
+.globl playerName
 
 .data
 playerName:	.space 64
-playerColor:	.space 4
+playerConfig:	.word 
 .text
 main:
 	jal  display_Menus
+	jal  zeroAll
 	jal  bitmapDisplay_Configuration
+	jal  zeroAll
 	jal  start_Game
 	jal  stop
+	
+#########################################################
+# Function que zera todos os valores de registradores	#
+#							#
+zeroAll:
+	li   $v0, 0
+	li   $v1, 0
+	li   $a0, 0
+	li   $a1, 0
+	li   $a2, 0
+	li   $a3, 0
+	li   $t1, 0
+	li   $t2, 0
+	li   $t3, 0
+	li   $t4, 0
+	li   $t5, 0
+	li   $t6, 0
+	li   $t7, 0
+	li   $s0, 0
+	li   $s1, 0
+	li   $s2, 0
+	li   $s3, 0
+	li   $s4, 0
+	li   $s5, 0
+	li   $s6, 0
+	li   $s7, 0
+	li   $t8, 0
+	li   $t9, 0
+	li   $sp, 0x7fffeffc
+	jr   $ra
 #########################################
-# Stop the program			#
+# Para o programa			#
 stop:
 	li   $v0, 17
 	syscall
