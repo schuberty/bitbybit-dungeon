205ed51a80cf391d9d05fdfb348dfab79ddd06f7
schubert <schubertyi@gmail.com>
2019-11-28 17:53:08 -0300

    Added all main menu options and functions

--- src/mainMenu.asm
+++ src/mainMenu.asm
@@ -1,4 +1,4 @@
-.globl display_Menus,MMIO_sendToDisplay,MMIO_GetChar
+.globl display_Menus,MMIO_sendToDisplay,MMIO_sendChar,MMIO_getChar,selectedOpt,printSelection
 
 .data
 stringWelcome:	.asciiz "        Bit by Bit: A Dungeon Game\n\n            Trabalho Final de AOC I\nDesenvolvido por: Gabriel Schubert M."
@@ -7,15 +7,23 @@ stringConfig:	.ascii  "    Settings do MARS necessárias,\nfora do padrão:\n\n1
 stringMMIO:	.ascii  "    Modo de uso do Keyboard MMIO Simulator:\n1. Teclas W e S para movimentação vertical;\n2. Teclas A e D para movimentação horizental;\n3. Tecla E para enter ou ação;\n4. Tecla Q para voltar ou parar movimentação."
 		.asciiz "\n\n                          BOM JOGO!"
 
-stringMenu:	.asciiz  "\tBit by Bit Dungeon MENU:\n\n"
+menuHeader:	.asciiz "\tBit by Bit Dungeon MENU:\n\n"
 menuOpt1:	.asciiz "\t   Entrar na Masmorra\n"
 menuOpt2:	.asciiz "\t Selecionar Dificuldade\n"
-menuOpt3:	.asciiz "\t      Sair do Menu"
+menuOpt3:	.asciiz "\t      Sair do Menu\n\n Dificuldade atual : "
 selectedOpt:	.asciiz "      >"
 
+diffMenuHeader:	.asciiz "\tSelecione a dificuldade:\n\n"
+diffMenuOpt1:	.asciiz "\t       Fácil\n"
+diffMenuOpt2:	.asciiz "\t       Médio\n"
+diffMenuOpt3:	.asciiz "\t      Díficil\n"
+diffMenuBack:	.ascii  "\t  Voltar pro menu"
+diffMenuSubt:	.asciiz "\n\n Fácil   : +50 de experiência ganha\n Médio   : Ganho normal de experiência\n Difícil : -50% de experiência ganha"
+diffEasy:	.asciiz "Fácil"
+diffNormal:	.asciiz "Médio"
+diffHard:	.asciiz "Difícil"
 
-
-playerGetName:	.asciiz "Diga-me, aventureiro de uma terra distante chamada Pelota"
+playerGetName:	.asciiz ""
 playerGetColor: .asciiz ""
 
 
@@ -27,145 +35,210 @@ display_Menus:
 	
 #	jal   msg_Config
 	jal   MMIO_MainMenu
-	jal   MMIO_GetPlayer
-	jal   msg_Prologue
 	
 	lw    $ra, ($sp)
 	addiu $sp, $sp, 4
 	jr    $ra
-	
-#########################################
-# Mostra 3 janelas de inicio		#
-msg_Config:				#
-	li    $v0, 55			# Valor de InputDialogInt
-	li    $a1, 1			# Valor de Information Window
-	la    $a0, stringWelcome	# Começa a mostrar as janelas
+#########################################################
+# -Mostra as três janelas pop-up de ínicio do programa	#
+msg_Config:						#
+	li    $v0, 55					# Valor de InputDialogInt
+	li    $a1, 1					# Valor de Information Window
+	la    $a0, stringWelcome			# Começa a mostrar as janelas
 	syscall
 	la    $a0, stringConfig
 	syscall
 	la    $a0, stringMMIO
 	syscall
-	jr    $ra
+	jr    $ra					# Retorna
 #########################################################
 # -Mostra o menu principal, para o jogador, com opções	#
 # de inciar a masmorra, selecionar dificuldade ou sair	#
-MMIO_MainMenu:				#
-	addiu $sp, $sp, -4		# Pilha pro retorno
+MMIO_MainMenu:
+	addiu $sp, $sp, -4				# Pilha para o retorno
 	sw    $ra, ($sp)
-	lui   $t0, 0xffff		# Valor do Transmitter Controller Ready bit
-	li    $t1, 1			# Posicionado para 1 manualmente
-	sb    $t1, 8($t0)		# E armazenado na posição especifica
-	li    $t2, 0			# Valor salvo pro Ready Transmitter
-	li    $t3, 1			# Valor inicial da opção do menu selecionada
+	lui   $t0, 0xffff				# Valor do Transmitter Controller Ready bit
+	li    $t1, 1					# Posicionado para 1 manualmente
+	sb    $t1, 8($t0)				# E armazenado na posição especifica
+	li    $t2, 0					# Valor salvo pro Ready Transmitter
+	li    $t3, 1					# Valor inicial da opção do menu selecionada
+	add   $t1, $t1, 11				# Incrementa para ser o valor 12 que da clear no MMIO Display
+	mainMenuSelection:
+		sw    $t1, 12($t0)			# Armazena o valor para dar clear
+		la    $a1, menuHeader			# Valor á enviar na MMIO Display
+		jal   MMIO_sendToDisplay
+#		sw    $zero, 4($t0)
+		li    $t4, 1				# Valor para checar se a seta vai nesta posição
+		jal   printSelection			# Imprime a seta, caso seja nesta posição
+		la    $a1, menuOpt1			# Carrega a opção do menu
+		jal   MMIO_sendToDisplay		# Envia a opção do menu
+		li    $t4, 2
+		jal   printSelection
+		la    $a1, menuOpt2
+		jal   MMIO_sendToDisplay	
+		li    $t4, 3
+		jal printSelection
+		la    $a1, menuOpt3
+		jal   MMIO_sendToDisplay
+		jal   printDifficult
+		# Mostra a dificuldade no menu principal
+		jal   MMIO_getChar			# Pega o char digitado pelo MMIO Keyboard
+		beq   $v0, 'e', mainKeyEnter		# Checa qual foi para executar uma ação
+		beq   $v0, 'E', mainKeyEnter
+		beq   $v0, 'w', mainKeyUp
+		beq   $v0, 'W', mainKeyUp
+		beq   $v0, 's', mainKeyDown
+		beq   $v0, 'S', mainKeyDown
+	j     mainMenuSelection				# Loop até o topo
+	
 
-	add   $t1, $t1, 11
-menuSelection:
-	sw    $t1, 12($t0)		# ASCII 12 da clear na MMIO Display
-	la    $a1, stringMenu		# Valor á enviar na MMIO Display
-	jal   MMIO_sendToDisplay
-#	sw    $zero, 4($t0)
-	li    $t4, 1
-	jal   printSelection
-	la    $a1, menuOpt1
-	jal   MMIO_sendToDisplay
-	li    $t4, 2
-	jal   printSelection
-	la    $a1, menuOpt2
-	jal   MMIO_sendToDisplay
-	li    $t4, 3
-	jal printSelection
-	la    $a1, menuOpt3
-	jal   MMIO_sendToDisplay
-	jal   MMIO_GetChar
-	beq   $v0, 'e', keyEnter
-	beq   $v0, 'E', keyEnter
-	beq   $v0, 'w', keyUp
-	beq   $v0, 'W', keyUp
-	beq   $v0, 's', keyDown
-	beq   $v0, 'S', keyDown
-	j     menuSelection
-
-keyDown:
-	bge   $t3, 3, menuSelection
-	add   $t3, $t3, 1
-	j     menuSelection
-keyUp:
-	ble   $t3, 1, menuSelection
+#-------#################################################
+	# -Ações dependendo do char selecionado		#
+	# @param $t3 : int, posição no menu		#
+	mainKeyDown:					# Posiciona a seta para baixo, se não for o limite
+	bge   $t3, 3, mainMenuSelection			# Se for o limite no menu
+	add   $t3, $t3, 1				# Se não for, incrementa o registrador do menu
+	j     mainMenuSelection				# Volta pro menu
+	mainKeyUp:					# Posiciona a seta para cima, se não for o limite
+	ble   $t3, 1, mainMenuSelection
 	add   $t3, $t3, -1
-	j     menuSelection
-changeDifficult:
-	j     menuSelection
-printSelection:
-	addiu $sp, $sp, -4
+	j     mainMenuSelection
+	mainKeyEnter:					# Executa a opção atual selecionada
+	beq   $t3, 2, MMIO_DifficultMenu		# Se for 2, menu de mudar dificuldade
+	beq   $t3, 3, stop				# Se for 3, saí do programa
+	lw    $ra, ($sp)				# Se for 1, entra na masmorra no nível 1
+	addiu $sp, $sp, 4				#
+	jr    $ra					# Retorna da pilha
+#########################################################
+# -Menu para mudar dificuldade almejada			#
+MMIO_DifficultMenu:
+	addiu $sp, $sp, -4				# Pilha para o retorno
 	sw    $ra, ($sp)
-
-	sub   $t4, $t4, $t3
-	bnez  $t4, printNext
-	la    $a1, selectedOpt
+	
+	li    $t3, 1
+	difficultSelection:				# Loop para selecionar a dificuldade
+		sw    $t1, 12($t0)			# Clear
+		la    $a1, diffMenuHeader
+		jal   MMIO_sendToDisplay
+		li    $t4, 1
+		jal   printSelection
+		la    $a1, diffMenuOpt1
+		jal   MMIO_sendToDisplay
+		li    $t4, 2
+		jal   printSelection
+		la    $a1, diffMenuOpt2
+		jal   MMIO_sendToDisplay
+		li    $t4, 3
+		jal   printSelection
+		la    $a1, diffMenuOpt3
+		jal   MMIO_sendToDisplay
+		li    $t4, 4
+		jal   printSelection
+		la    $a1, diffMenuBack
+		jal   MMIO_sendToDisplay
+		jal   MMIO_getChar			# Pega o char digitado pelo MMIO Keyboard
+		beq   $v0, 'e', diffKeyEnter		# Checa qual foi para executar uma ação
+		beq   $v0, 'E', diffKeyEnter
+		beq   $v0, 'w', diffKeyUp
+		beq   $v0, 'W', diffKeyUp
+		beq   $v0, 's', diffKeyDown
+		beq   $v0, 'S', diffKeyDown
+			
+	j     difficultSelection
+	diffBackOption:					# Volta para o menu principal
+	li    $t3, 1					# Local da seta no menu principal
+	lw    $ra, ($sp)				# Atualiza a pilha
+	addiu $sp, $sp, 4
+	j     mainMenuSelection				# Retorna pro menu principal
+#-------#################################################
+	# -Ações dependendo do char selecionado		#
+	# @param $t3 : int, posição no menu		#
+	diffKeyDown:					# Posiciona a seta para baixo, se não for o limite
+	bge   $t3, 4, difficultSelection		# Se for o limite no menu
+	add   $t3, $t3, 1				# Se não for, incrementa o registrador do menu
+	j     difficultSelection			# Volta pro menu
+	diffKeyUp:					# Posiciona a seta para cima, se não for o limite
+	ble   $t3, 1, difficultSelection
+	add   $t3, $t3, -1
+	j     difficultSelection
+	diffKeyEnter:					# Executa a opção atual selecionada
+	beq   $t3, 4, diffBackOption			# Volta para o menu principal
+	la    $t4, gameSettings				# Carrega local da configuração
+	sw    $t3, 0($t4)				# Salva a dificuldade na configuração
+	j     diffBackOption				# Muda a dificuldade
+#-------#################################################
+	# -Mostra a dificuldade no menu principal	#
+	# @param $t3 : int, posição no menu		#
+	printDifficult:
+	addiu $sp, $sp, -4				# Pilha para o retorno
+	sw    $ra, ($sp)
+	la    $t4, gameSettings
+	lw    $t4, 0($t4)
+	beq   $t4, 1, printEasy
+	beq   $t4, 3, printHard
+	la    $a1, diffNormal
 	jal   MMIO_sendToDisplay
-printNext:
+	j     diffNext
+	printEasy:
+	la    $a1, diffEasy
+	jal   MMIO_sendToDisplay
+	j     diffNext
+	printHard:
+	la    $a1, diffHard
+	jal   MMIO_sendToDisplay
+	diffNext:
 	lw    $ra, ($sp)
 	addiu $sp, $sp, 4
-	jr    $ra
-keyEnter:
-	beq   $t3, 2, changeDifficult
-	beq   $t3, 3, stop
-	lw    $ra, ($sp)		# Retorno da pilha
+	jr    $ra					# Retorno da pilha
+#########################################################
+# -Imprime a seta na opção selecionada			#
+# @param $t3 : int, posição atual no menu		#
+# @param $t4 : int, posição á testar			#
+printSelection:
+	addiu $sp, $sp, -4				# Pilha para o retorno
+	sw    $ra, ($sp)
+	sub   $t4, $t4, $t3				# Vê se o valor em $t4 equivale ao do reg. onde a opção está
+	bnez  $t4, printNext				# Se não for, continua para voltar
+	la    $a1, selectedOpt				# Se for, imprime a seta na opção selecionada
+	jal   MMIO_sendToDisplay			# E envia pro display
+	printNext:
+	lw    $ra, ($sp)
 	addiu $sp, $sp, 4
-	jr    $ra
-	
+	jr    $ra					# Retorno da pilha
 #########################################################
 # -Percorre uma string para enviar ao MMIO Display	#
 # @param $a1 : string 					#
-MMIO_sendToDisplay:			#
-	addiu $sp, $sp, -4		# Pilha pro retorno
+MMIO_sendToDisplay:					#
+	addiu $sp, $sp, -4				# Pilha para o retorno
 	sw    $ra, ($sp)
-toDisplayLoop:
-	lb    $a0, ($a1)		# Carrega char da string
-	jal   MMIO_SendChar		# Envia
-	addi  $a1, $a1, 1		# Proximo char da strings
-	bnez  $a0, toDisplayLoop	# Enquanto não for fim da string
-	lw    $ra, ($sp)		# Retorna da pilha
-	addiu $sp, $sp, 4
-	jr    $ra
-
-#########################################################
-# Envia o caractere especificado para o MMIO Display	#
-# @param $a0 : char, valor ASCII			#
-MMIO_SendChar:				#
-	lui   $a3, 0xffff		# Carrega o endereço base
-sendCursor:				# Loop desabilitado, Transmitter bit habilitado em MMIO_MainMenu
-	lw    $t2, 8($a3)		# Valor do Transmitter Controller Ready bit
-	andi  $t2, $t2, 1		# Habilita a escrita no MMIO Display
-	beqz  $t2, sendCursor		# Espera primeiro input pra estar disponível (RESET)
-	sw    $a0, 12($a3)		# Envia o char pra posição de envio
-	jr    $ra			# Retorna
-#########################################################
-# -Intercepta um caractere enviado pelo MMIO Keyboard	#
-# @return $v0 : char, valor ASCII			#							#
-MMIO_GetChar:
-	lui   $a1, 0xffff
-inputReady:
-	lw    $a2, 0($a1)
-	andi  $a2, $a2, 0x1
-	beqz  $a2, inputReady
-	lw    $v0, 4($a1)
-	jr    $ra
-#########################################################
-# -Intercepta um caractere enviado pelo MMIO Keyboard	#
-# @return $v0 : char, valor ASCII		#
-MMIO_GetPlayer:
-	addiu $sp, $sp, -4
-	sw    $ra, ($sp)
-	
-getNameLoop:
-
-	
+	toDisplayLoop:
+		lb    $a0, ($a1)			# Carrega char da string
+		jal   MMIO_sendChar			# Envia
+		addi  $a1, $a1, 1			# Proximo char da strings
+	bnez  $a0, toDisplayLoop			# Enquanto não for fim da string
 	lw    $ra, ($sp)
 	addiu $sp, $sp, 4
-	jr    $ra
+	jr    $ra					# Retorna da pilha
+#-------#################################################
+	# -Envia o caractere para o MMIO Display	#
+	# @param $a0 : char, valor ASCII		#
+	MMIO_sendChar:	
+		lui   $a3, 0xffff			# Carrega o endereço base do MMIO
+		sendCursor:
+			lw    $a2, 8($a3)		# Valor do Transmitter Controller Ready bit
+			andi  $a2, $a2, 1		# Habilita a escrita no MMIO Display
+		beqz  $a2, sendCursor			# Espera primeiro input pra estar disponível (RESET)
+		sw    $a0, 12($a3)			# Envia o char pra posição de envio
+		jr    $ra				# Retorna
+#-------#################################################
+	# -Intercepta um char pelo MMIO Keyboard	#
+	# @param $v0 : char, valor ASCII		#
+	MMIO_getChar:
+		lui   $a1, 0xffff			# Carrega o endereço base
+		inputReady:
+			lw    $a2, 0($a1)		# Carrega o valor que checa se o MMIO Keyboard está
+			andi  $a2, $a2, 0x1		# pronto para leitura de um char
+		beqz  $a2, inputReady			# Se não estiver, volta até estar (método sem input lag)
+		lw    $v0, 4($a1)			# Salva o char digitado em $v0
+		jr    $ra				# Retorna
 #########################################################
-# -Intercepta um caractere enviado pelo MMIO Keyboard	#
-# @return $v0 : Valor ASCII do caractere		#
-msg_Prologue:
-	jr    $ra
