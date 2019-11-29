6bec5ec2f613b2a1d3c02b63714fc974e930fcff
schubert <schubertyi@gmail.com>
2019-11-29 00:43:19 -0300

    Added map generation and next room functions

--- src/bitmapConfig.asm
+++ src/bitmapConfig.asm
@@ -1,9 +1,8 @@
-.globl bitmapDisplay_Configuration,genNextRoom
+.globl bitmapDisplay_Configuration,genNextRoom,nextRoom
 
 .data
-HPColor:	.word 0xFF0000, 0xD3A4A4
-MPColor:	.word 0x000FFF, 0xAAAFFF
-
+actualRmColor:	.word 0xFFF93F
+endedRmColor:	.word 0x53FF3F
 .text
 bitmapDisplay_Configuration:
 	addiu $sp, $sp, -4
@@ -12,6 +11,7 @@ bitmapDisplay_Configuration:
 	jal   gen_Background
 	lui   $s0, 0x1004				# Bitmap Display address
 	addi  $s0, $s0, 4112				# Spawn inicial do player
+	jal   gen_Map
 	genNextRoom:
 	jal   gen_Room
 	
@@ -47,15 +47,42 @@ gen_Background:
 	blt   $t4, 2076, bgLoop				# Checa se preencheu toda a area do mapa
 	jr    $ra					# Retorna
 #########################################################
+# Gera o mapa no topo do Bitmap Display			#
+gen_Map:
+	lui   $t0, 0x1004
+	addi  $t0, $t0, 272				# Base address
+	li    $t3, 2					# Numero de linhas que haveram o mapa
+	lw    $t7, actualRmColor			# Cor da sala atual
+	li    $t8, 0xFF0000				# Cor da sala final
+	li    $t9, 0xFF7165				# Cor de salas seguintes
+
+	mapLoop1:
+		sw    $t7, ($t0)			# Desenha a sala atual inicial
+		sw    $t7, 4($t0)
+		li    $t2, 17
+		mapLoop2:
+			add   $t0, $t0, 12		# Desenha as salas intermediarias
+			sw    $t9, ($t0)
+			sw    $t9, 4($t0)
+			add   $t2, $t2, -1
+		bnez  $t2, mapLoop2
+		add   $t0, $t0, 12			# Desenha a sala final
+		sw    $t8, ($t0)
+		sw    $t8, 4($t0)
+		add   $t3, $t3, -1
+		add   $t0, $t0, 40			# Incrementa para a proxima linha
+	bnez  $t3, mapLoop1				# Se não for a ultima linha
+	jr    $ra					# Retorna
+#########################################################
 # Gera o chão e as paredas da sala			#
 gen_Room:
-	li    $t0, 0x10040000				# Buffer do Bitmap Display
-	add   $t0, $t0, 1032				# Incrementa para inicio da segunda linha da sala
+	lui    $t0, 0x1004				# Buffer do Bitmap Display
+	add   $t0, $t0, 1288				# Incrementa para inicio da segunda linha da sala
 	li    $t1, 0x47475D				# Cor da parede da sala
 	li    $t2, 0xB8B06C				# Cor de chão normal
 	li    $t3, 0xA59E62				# Cor de chão no escuro
 	li    $t4, 8					# Dividor do random int gerado é a probabilidade de ter chão escuro [2,10]
-	li    $t6, 25					# Quantidade de linhas para chão entre as paredes laterais
+	li    $t6, 24					# Quantidade de linhas para chão entre as paredes laterais
 	roomLoop1:
 		sw    $t1, ($t0)			# Pixel da primeira parede lateral esquerda
 		add   $t0, $t0, 4			# Incrementa para começar á desenhar o chão
@@ -65,7 +92,7 @@ gen_Room:
 			syscall
 			div   $a0, $t4
 			mfhi  $a0
-			beqz  $a0, roomShadowColor	# Pula para desenhar chão no escuro
+			beqz  $a0, roomShadowColor	# Pula para desenhar chão com sombra
 			sw    $t2, ($t0)		# Desenha chão com luminosidade
 			j    roomNext
 			roomShadowColor:
@@ -87,9 +114,51 @@ gen_Room:
 			add   $t0, $t0, 4
 		bnez  $t5, roomLoop4			# Após terminar a inferior
 		add   $t6, $t6, -1
-		add   $t0, $t0, -6896			# Decrementa para desenhar a parede superior
+		add   $t0, $t0, -6640			# Decrementa para desenhar a parede superior
 	bnez  $t6, roomLoop3				# Se completou as duas paredes horizontais
 	jr    $ra					# Retorna
-#########################################################
-# Gera os inimigos na sala atual			#
-
+#-------#################################################
+	# -Atualiza dados e o mapa para a próxima sala	#
+	nextRoom:
+	lui    $t0, 0x1004				# Buffer do Bitmap Display
+	add   $t0, $t0, 260				# Incrementa para 12 bits antes da primeira unidade da primeira sala
+	lw    $t1, gameSettings+4			# Sala atual
+	mul   $t1, $t1, 12				# Multiplica para pegar o endereço da sala atual
+	add   $t0, $t0, $t1				# Adiciona ao buffer para selecionar a sala atual
+	lw    $t8, actualRmColor			# Cor de sala atual
+	lw    $t9, endedRmColor				# Cor de sala completada
+	sw    $t9, ($t0)				# Começa a desenhar a sala completada
+	sw    $t9, 4($t0)
+	sw    $t9, 256($t0)
+	sw    $t9, 260($t0)
+	add   $t0, $t0, 12
+	sw    $t8, ($t0)				# Começa a desenhar a sala atual
+	sw    $t8, 4($t0)
+	sw    $t8, 256($t0)
+	sw    $t8, 260($t0)
+	lw    $t1, gameSettings+4
+	add   $t1, $t1, 1
+	sw    $t1, gameSettings+4
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	jr    $ra
+	
+	
+	
+	
+	
